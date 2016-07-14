5750af08535cf7bbf17d7896a15eef52de814cda
jonarj <jjonar7@gmail.com>
2016-07-13 20:48:20 -0500

    Add files via upload
    Se agrega los servicios de consulta impuestos SAT

---
+++ WCFServicioSOAT/IImpuesto_Vehicular.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCF_SAT_Impuesto_Vehicular.Dominio;
+using WCF_SAT_Impuesto_Vehicular.Errores;
+
+namespace WCF_SAT_Impuesto_Vehicular
+{
+    
+    [ServiceContract]
+    public interface IImpuesto_Vehicular
+    {
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        Sat ConsultaImpuesto(string placa);
+    }
+}
---
+++ WCFServicioSOAT/Impuesto_Vehicular.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCF_SAT_Impuesto_Vehicular.Persistencia;
+using WCF_SAT_Impuesto_Vehicular.Dominio;
+using WCF_SAT_Impuesto_Vehicular.Errores;
+
+namespace WCF_SAT_Impuesto_Vehicular
+{
+    
+    public class Impuesto_Vehicular : IImpuesto_Vehicular
+    {
+        private SatDAO satDAO = new SatDAO();
+
+        public Sat ConsultaImpuesto(string placa)
+        {
+
+            if (satDAO.ConsultaImpuesto(placa) == null)
+            {
+                throw new FaultException<RepetidoException>(
+                    new RepetidoException()
+                    {
+                        Codigo = "101",
+                        Descripcion = "No existe impuesto Vehicular para la placa ingresada"
+                    },
+                    new FaultReason("No existe impuesto Vehicular para la placa ingresada"));
+            }
+                return satDAO.ConsultaImpuesto(placa);   
+
+        }
+    }
+}
